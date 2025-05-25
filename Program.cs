ed681e7101f889a9bb60f6f1358530a559365e76
eXmaS <76765030+eXmaS@users.noreply.github.com>
2025-05-25 08:14:56 +0300

    ok

--- Program.cs
+++ Program.cs
@@ -19,6 +19,11 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
+app.MapGet("/", () =>
+{
+    return Results.Ok();
+});
+
 app.MapGet("/weatherforecast", () =>
     {
         var forecast = Enumerable.Range(1, 5)
